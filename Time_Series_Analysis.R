51a531cbc87d8f3026c91ab48560e8e2ba3989ea
Erica Genoni <127137914+ericagenoni@users.noreply.github.com>
2024-09-09 12:19:31 +0200

    Update Time_Series_Analysis.R

--- Time_Series_Analysis.R
+++ Time_Series_Analysis.R
@@ -1,33 +1,29 @@
-#TIME SERIES ANALYSIS
+# ANALISI DELLE SERIE TEMPORALI
 
-# Install the required packages
+# Installazione dei pacchetti necessari
 install.packages("rasterVis")
 install.packages("rgdal")
 
-# Load the required packages
+# Caricamento dei pacchetti necessari
 library(raster)
 library(rasterVis)
 library(rgdal)
 
+# 1. AUMENTO DELLE TEMPERATURE IN GROENLANDIA
 
-
-
-
-# 1. GREENLAND INCRESE OF TEMPERATURES
-
-# Set the working directory
+# Impostazione della directory di lavoro
 setwd("C:/Data_telerilevamento/lab/greenland_data")
 
-# Import .tif image with raster() function
+# Importazione delle immagini .tif utilizzando la funzione raster()
 lst_2000 <- raster("lst_2000.tif")
 lst_2005 <- raster("lst_2005.tif")
 lst_2010 <- raster("lst_2010.tif")
 lst_2015 <- raster("lst_2015.tif")
 
-# See the list of object imported/loaded
+# Visualizzazione della lista degli oggetti importati/caricati
 ls()
 
-# Plot in a multiframe the 4 images
+# Visualizzazione delle 4 immagini in un unico frame multipezzo
 par(mfrow=c(2,2))
 plot(lst_2000)
 plot(lst_2005)
@@ -35,104 +31,100 @@ plot(lst_2010)
 plot(lst_2015)
 dev.off()
 
-# List of files
+# Elenco dei file .tif
 rlist_1 <- list.files(pattern="lst")
 rlist_1
 
-# Apply a function over a list/vector
-import_1 <- lapply(rlist_1, raster) # Apply a function to many files of a list
+# Applicazione di una funzione su una lista/vector
+import_1 <- lapply(rlist_1, raster) # Applicazione della funzione raster() ai file della lista
 import_1
 
-# Stack vectors from a dataframe/list
+# Creazione di uno stack di layer da un dataframe/lista
 TGr <- stack(import_1)
-TGr # 4 layesr
-# Stacking vectors concatenates multiple vectors into a single vector
+TGr # 4 layer
+# Lo stacking concatena più layer in un unico layer
 
-# Plot TGr vector
-plot(TGr) # now we have the 4 images in a single element
+# Visualizzazione dello stack TGr
+plot(TGr) # ora abbiamo le 4 immagini in un unico elemento
 
-# Plot with plotRGB()
+# Visualizzazione con plotRGB()
 plotRGB(TGr, 1, 2, 3, stretch="Lin")
 plotRGB(TGr, 2, 3, 4, stretch="Lin")
 
-# Plot using a colour palette
+# Visualizzazione utilizzando una palette di colori
 cl_0 <- colorRampPalette(c("blue","lightblue","pink","red"))(100)
 plot(TGr, col=cl_0)
 dev.off()
 
-# Calculate and plot the differences between 2005 and 2000
+# Calcolo e visualizzazione delle differenze tra 2005 e 2000
 dif_05_00 = TGr[[2]] - TGr[[1]]
 plot(dif_05_00, col=cl_0)
 dev.off()
 
+# 2. DIMINUZIONE DI NO2 DURANTE IL PERIODO DI LOCKDOWN
 
-
-
-
-# 2. NO2 DECREASE DURING LOCKDOWN PERIOD
-
-# Set the new working directory
+# Impostazione della nuova directory di lavoro
 setwd("C:/Data_telerilevamento/lab/EN")
 
-# Import and plot the first image file
+# Importazione e visualizzazione del primo file immagine
 EN01 <- raster("EN_0001.png")
 EN01
-plot(EN01) # Jenuary (before lockdown began)
+plot(EN01) # Gennaio (prima dell'inizio del lockdown)
 
-# Plot EN01 using colour palette
+# Visualizzazione di EN01 utilizzando una palette di colori
 cl_1 <- colorRampPalette(c("yellow","orange","red"))(100)
 plot(EN01, col=cl_1)
 
-# Import and plot the last image file
+# Importazione e visualizzazione dell'ultimo file immagine
 EN13 <- raster("EN_0013.png")
 EN13
-plot(EN13, col=cl_1) # March (after lockdown began)
+plot(EN13, col=cl_1) # Marzo (dopo l'inizio del lockdown)
 
-# Import the whole set
+# Importazione dell'intero set di immagini
 rlist_2 <- list.files(pattern="EN")
 rlist_2
 
-# Apply a function over a list/vector
-import_2 <- lapply(rlist_2, raster) # Apply a function to many files of a list
+# Applicazione di una funzione su una lista/vector
+import_2 <- lapply(rlist_2, raster) # Applicazione della funzione raster() ai file della lista
 import_2
 
-# Stack vectors from a dataframe/list
+# Creazione di uno stack di layer da un dataframe/lista
 EN <- stack(import_2)
 EN
-# Stacking vectors concatenates multiple vectors into a single vector
+# Lo stacking concatena più layer in un unico layer
 
-# Plot all the image files
+# Visualizzazione di tutte le immagini
 plot(EN, col=cl_1)
 
-# Check by visualizing the plots in a multiframe
+# Verifica visualizzando i plot in un frame multipezzo
 par(mfrow=c(1,2))
 plot(EN[[1]], col=cl_1)
 plot(EN01, col=cl_1)
 dev.off()
 
-# Check by looking for differences
+# Verifica calcolando le differenze
 dif_check <- EN01 - EN[[1]]
 dif_check
 plot(dif_check)
 
-# Plot the first and the last images with par() function
+# Visualizzazione del primo e dell'ultimo file immagine con funzione par()
 par(mfrow=c(1,2))
 plot(EN01, col=cl_1)
 plot(EN13, col=cl_1)
 dev.off()
 
-# Plot the first and the last images with stack() function
+# Creazione di uno stack tra il primo e l'ultimo file immagine
 EN_01_13 <- stack(EN01, EN13)
 
-# Calculate the differences between EN01 and EN13
+# Calcolo delle differenze tra EN01 e EN13
 dif_01_13 <- EN01 - EN13
 cl_2 <- colorRampPalette(c("blue","white","red"))(100)
 
-# Plot the differences between EN01 and EN13 with plot()
+# Visualizzazione delle differenze tra EN01 e EN13
 plot(dif_01_13, col=cl_2)
 dev.off()
 
-# Plot with plotRGB()
+# Visualizzazione con plotRGB()
 par(mfrow=c(1,2))
 plotRGB(EN, 1, 7, 13, stretch="lin")
 plotRGB(EN, 1, 7, 13, stretch="hist")
