0b8e40d3b217fb3967fbd6d16089e403957bbc45
Erica Genoni <127137914+ericagenoni@users.noreply.github.com>
2024-09-09 11:59:47 +0200

    Update R_code_LiDAR.R

--- R_code_LiDAR.R
+++ R_code_LiDAR.R
@@ -1,151 +1,128 @@
 # LiDAR - 3D in R
 
-# LiDAR is a method for determining ranges by targeting an object oa a surface
-# with a laser and measuring the time for the reflected light to return to the
-# receiver. It may scan multiple directions resulting in a 3D image
+# LiDAR è una tecnologia per determinare le distanze tramite il targeting di un oggetto o di una superficie
+# con un laser e la misurazione del tempo che la luce riflessa impiega per ritornare al ricevitore.
+# Può scansionare in più direzioni, risultando in un'immagine 3D.
 
-# Load the required packages
-library(raster) # Geographic Data Dnalysis and Modelling
-library(rgdal) # Geospatial Data Abstraction Library
-library(viridis)
-library(ggplot2)
-library(patchwork)
+# Caricamento dei pacchetti richiesti
+library(raster) # Analisi e modellizzazione di dati geografici
+library(rgdal) # Libreria di astrazione dei dati geospaziali
+library(viridis) # Libreria per la scala di colori
+library(ggplot2) # Libreria per la visualizzazione dei dati
+library(patchwork) # Libreria per combinare più grafici
 
-# Set the working directory
+# Impostazione della directory di lavoro
 setwd("C:/Data_telerilevamento/lab/dati")
-
-
-
-
 
 # 2013
 
-# Load data: Digital Surface Model 2013
+# Caricamento dei dati: Modello digitale di superficie 2013
 dsm_2013 <- raster("2013Elevation_DigitalElevationModel-0.5m.tif")
 
-# Visualize info about the loaded data
+# Visualizzazione delle informazioni sui dati caricati
 dsm_2013
 
-# Plot DSM 2013
+# Creazione di un plot per il DSM 2013
 plot(dsm_2013, main="LiDAR Digital Surface Model San Genesio/Jenesien")
 
-
-
-# Load data: Digital Terrain Models 2013
+# Caricamento dei dati: Modello digitale del terreno 2013
 dtm_2013 <- raster("2013Elevation_DigitalTerrainModel-0.5m.tif")
 
-# Visualize info about the loaded data
+# Visualizzazione delle informazioni sui dati caricati
 dtm_2013
 
-# Plot DTM 2013
+# Creazione di un plot per il DTM 2013
 plot(dtm_2013, main="LiDAR Digital Terrain Model San Genesio/Jenesien")
 
-
-
-# Create CHM 2013 as difference between DSM and DTM
+# Creazione del Canopy Height Model (CHM) 2013 come differenza tra DSM e DTM
 chm_2013 <- dsm_2013 - dtm_2013
 
-# Visualize info about CHM
+# Visualizzazione delle informazioni sul CHM
 chm_2013
 
-# Coerce CHM 2013 into a dataframe
+# Conversione del CHM 2013 in un dataframe
 chm_2013_df <- as.data.frame(chm_2013, xy=T)
 
-# Plot CHM with ggplot()
+# Creazione di un plot del CHM con ggplot
 ggplot() +
   geom_raster(chm_2013_df,
               mapping=aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis() +
   ggtitle("CHM 2013 San Genesio/Jenesien")
 
-# Save CHM 2013 on PC
+# Salvataggio del CHM 2013 sul PC
 writeRaster(chm_2013, "CHM_2013_San_Genesio/Jenesien.tif")
-
-
-
-
 
 # 2004
 
-# Load data: Digital Surface Model 2004
+# Caricamento dei dati: Modello digitale di superficie 2004
 dsm_2004 <- raster("2004Elevation_DigitalElevationModel-2.5m.tif")
 
-# Visualize info about the loaded data
+# Visualizzazione delle informazioni sui dati caricati
 dsm_2004
 
-# Plot DSM 2004
+# Creazione di un plot per il DSM 2004
 plot(dsm_2004, main="LiDAR Digital Surface Model San Genesio/Jenesien")
 
-
-
-# Load data: Digital Terrain Models 2004
+# Caricamento dei dati: Modello digitale del terreno 2004
 dtm_2004 <- raster("2004Elevation_DigitalTerrainModel-2.5m.tif")
 
-# Visualize info about the loaded data
+# Visualizzazione delle informazioni sui dati caricati
 dtm_2004
 
-# Plot DTM 2004
+# Creazione di un plot per il DTM 2004
 plot(dtm_2013, main="LiDAR Digital Terrain Model San Genesio/Jenesien")
 
-
-
-# Create CHM 2004 as difference between DSM and DTM
+# Creazione del Canopy Height Model (CHM) 2004 come differenza tra DSM e DTM
 chm_2004 <- dsm_2004 - dtm_2004
 
-# Visualize info about CHM
+# Visualizzazione delle informazioni sul CHM
 chm_2004
 
-# Coerce CHM 2004 into a dataframe
+# Conversione del CHM 2004 in un dataframe
 chm_2004_df <- as.data.frame(chm_2004, xy=T)
 
-# Plot CHM with ggplot()
+# Creazione di un plot del CHM con ggplot
 ggplot() +
   geom_raster(chm_2004_df,
               mapping=aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis() +
   ggtitle("CHM 2004 San Genesio/Jenesien")
 
-# Save CHM 2004 on PC
+# Salvataggio del CHM 2004 sul PC
 writeRaster(chm_2004, "CHM_2004_San_Genesio/Jenesien.tif")
 
+# Calcolo della differenza tra i CHM
+difference <- chm_2013 - chm_2004 # ERRORE
 
-
-
-
-# Difference between CHM
-difference <- chm_2013 - chm_2004 # ERROR
-
-# Resample 2013 to 2004 (2.5 m)
+# Risampling del CHM 2013 per adattarlo al 2004 (2.5 m)
 chm_2013_rs <- resample(chm_2013, chm_2004)
 
-# Difference between CHM
+# Calcolo della differenza tra i CHM
 difference <- chm_2013_rs - chm_2004
 
-# Coerce the difference into a dataframe
+# Conversione della differenza in un dataframe
 difference_df <- as.data.frame(difference, xy=T)
 
-# Plot the difference between CHM
+# Creazione di un plot della differenza tra i CHM
 ggplot() +
   geom_raster(difference_df,
               mapping=aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis() +
   ggtitle("Difference CHM San Genasio/Jenesien")
 
-# Save CHM 2013 on PC
+# Salvataggio della differenza dei CHM sul PC
 writeRaster(difference, "CHM_San_Genesio/Jenesien.tif")
-
-
-
-
 
 # POINT CLOUD
 
-# Load the required package
+# Installazione del pacchetto richiesto
 install.packages("lidR")
 library(lidR)
 
-# Load Point Cloud
+# Caricamento del Point Cloud
 point_cloud <- readLAS("point_cloud.laz")
 
-# Plot r3 Point Cloud
+# Creazione di un plot per il Point Cloud
 plot(point_cloud)
+
